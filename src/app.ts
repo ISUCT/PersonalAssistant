8a59a505ddaaccd13f5622574ea8c254d6b14407
jskonst <jskonst@yandex.ru>
2019-03-28 08:54:59 +0300

    Changed settings to start from vscode

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express = require("express");
+import express = require('express');
 const app = express();
 const port = 3000; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req: any, res: any ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (req: any, res: any) => {
+    res.send('Hello world!');
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${ port }`);
+});
