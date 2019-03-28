0476416ffdd39fdd436ee5e4ec33356b78f14929
jskonst <jskonst@yandex.ru>
2019-03-28 09:19:42 +0300

    Added sample of second controller

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,14 @@ app.get('/', (req: any, res: any) => {
     res.send('Hello world!');
 });
 
+app.get('/test', (req: any, res: any) => {
+    res.send({
+        data: ['Test data!',
+            'Test data1', 'Test data2'],
+        date: new Date(),
+    });
+});
+
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${ port }`);
